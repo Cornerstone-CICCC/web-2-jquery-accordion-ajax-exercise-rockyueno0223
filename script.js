47670289074ec59208d3b85b1218dfb4f2857619
Hiroki Ueno <rockyueno@gmail.com>
2024-07-31 16:23:23 -0700

    submit

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $(function() {
   // your code here
+  $('.accordion-header').on('click', function () {
+    $(this).next().slideToggle(500);
+    $(this).siblings("div").not($(this).next()).slideUp();
+  })
+
+  $('.todos button').on('click', function () {
+    $.ajax({
+      url: 'https://dummyjson.com/todos',
+      type: 'GET',
+      success: function (response) {
+        const data = response.todos;
+        data.forEach(todo => {
+          $('.todos button').next().append(`<li>${todo.todo}</li>`);
+        });
+      },
+      error: function (error) {
+        console.error(error);
+      }
+    })
+  })
 })
